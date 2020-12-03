00ee2cf493eeb0514c6e3b6f660b437eb5f0e016
Sobii <moradiyan.sobhan@gmail.com>
2020-12-03 11:17:36 +0330

    query step ..

--- searchEngine.py
+++ searchEngine.py
@@ -1,4 +1,5 @@
 from elasticsearch import Elasticsearch
+import json
 
 es = Elasticsearch(HOST="http://localhost", PORT=9200)
 es = Elasticsearch()
@@ -10,36 +11,20 @@ def docIndexing():
             doc = json.loads(line[0:len(line)-2])
             es.index(index="news", doc_type="content", id=doc_id, body=doc)
             doc_id = doc_id + 1
-            print(doc)
 
 docIndexing()
 
 
-# doc_1 = {"sentence":"Hack COVID-19 is amazing!"}
-# doc_2 = {"sentence":"Hack-Quarantine is stunning!"}
-#
-# es.index(index="english", doc_type="sentences", id=1, body=doc_1)
-# es.index(index="english", doc_type="sentences", id=2, body=doc_2)
-#
-#
-#
-# body = {
-#     "from":0,
-#     "size":2,
-#     "query": {
-#         "bool": {
-#             "must_not": {
-#                 "match": {
-#                     "sentence":"COVID-19"
-#                 }
-#             },
-#             "should": {
-#                 "match": {
-#                     "sentence": "Hack"
-#                 }
-#             }
-#         }
-#     }
-# }
-# res = es.search(index="english", body=bo
-# print(res)
+
+body={
+    "from": 0,
+    "size": 500,
+    "query": {
+        "match": {
+            "content": "trump"
+        }
+    }
+}
+
+res = es.search(index="news", body=body)
+print(res)
